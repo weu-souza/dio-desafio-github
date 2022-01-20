3a0c19cf55400408c9def102759cc540bb6f0d0e
weu-souza <weuller1561@gmail.com>
2022-01-19 23:29:05 -0300

    ...

---
+++ JavaScriptDio/contador/assets/js/scripts.js
@@ -0,0 +1,33 @@
+let currentNumberWrapper = document.getElementById('currentNumber');
+let subtrair = document.getElementById('subtrair');
+let adicionar = document.getElementById('adicionar');
+let currentNumber = 1;
+currentNumberWrapper.innerHTML=currentNumber;
+
+function increment(){
+    if(currentNumber >= 9){
+        adicionar.disabled = true;
+        adicionar.style.color = '#000000';
+        adicionar.style.border = '2px solid #D2D1D100';
+        adicionar.style.backgroundColor = '#D2D1D1FF';
+    } else if(!(currentNumber  > 0)) {
+        subtrair.disabled = false;
+        subtrair.style.color = '#F08080FF';
+        subtrair.style.border = '2px solid #F08080FF';
+    }
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML=currentNumber;
+}
+function decrement(){
+    currentNumber = currentNumber -1;
+    currentNumberWrapper.innerHTML=currentNumber;
+    if(currentNumber <= 0){
+        subtrair.disabled = true;
+        subtrair.style.color = '#FDFDFD';
+        subtrair.style.border = '2px solid #FDFDFD';
+    } else if(!(currentNumber  < 10)) {
+        adicionar.disabled = false;
+        adicionar.style.color = '#F08080FF';
+        adicionar.style.border = '2px solid #F08080FF';
+    }
+}
